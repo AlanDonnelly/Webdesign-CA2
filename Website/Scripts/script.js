3e079fd8d085792133f4709e21ba8d2d4b723f33
Jesse G <157631561+xJG24@users.noreply.github.com>
2024-04-19 09:47:13 +0100

    FIXED merge issues
    -Overflow-x hidden on generic body style
    - Splash Images display fixed and responsive fix
    - SLight JS tweaks (mostly ideneting and styling)

--- Website/Scripts/script.js
+++ Website/Scripts/script.js
@@ -110,35 +110,35 @@ function filterInstruments(category) {
     } else {
         return filteredList = INSTRUMENT_PRODUCTS_LIST.filter(product => product.category === category)
     }
-  }
-  
-  function filterAudio(category) {
+}
+
+function filterAudio(category) {
     let filteredList;
     if (category === 'all') {
         return filteredList = AUDIO_PRODUCTS_LIST;
     } else {
         return filteredList = AUDIO_PRODUCTS_LIST.filter(product => product.category === category)
     }
-  }
-  
-  //creates cards which are styled as horizontal using a mixture of BS styles and CSS via ID selectors
-  
-  //clears list and selects correct area of the dom
-  function renderProductsInstruments(filteredList = filterInstruments(category)) {
+}
+
+//creates cards which are styled as horizontal using a mixture of BS styles and CSS via ID selectors
+
+//clears list and selects correct area of the dom
+function renderProductsInstruments(filteredList = filterInstruments(category)) {
     const productList = document.getElementById('product-list');
     productList.innerHTML = '';
-  
+
     //Creates the cards for each item and appends each section to the card before adding the card to the DOM product-list
     filteredList.forEach(product => {
         let productCard = document.createElement('div');
         productCard.id = 'product-card';
         productCard.classList.add('card', 'mb-2', 'mt-1');
         productCard.style.height = '200px';
-  
+
         //to allow the use of BS columns (used a large gutter to stop text clipping issues)
         let cardRow = document.createElement('div');
         cardRow.classList.add('row', 'g-10');
-  
+
         // creating the image and using innerHTML to set the src attribute of the Img tag
         let cardImage = document.createElement('div');
         cardImage.id = 'card-image';
@@ -165,26 +165,26 @@ function filterInstruments(category) {
         });
         productList.appendChild(productCard);
     });
-  }
-  
-  function renderProductsAudio(filteredList = filterAudio(category)) {
+}
+
+function renderProductsAudio(filteredList = filterAudio(category)) {
     const productList = document.getElementById('product-list');
     productList.innerHTML = '';
-  
+
     filteredList.forEach(product => {
         let productCard = document.createElement('div');
         productCard.id = 'product-card';
         productCard.classList.add('card', 'mb-2', 'mt-1');
         productCard.style.height = '200px';
-  
+
         let cardRow = document.createElement('div');
         cardRow.classList.add('row', 'g-10');
-  
+
         let cardImage = document.createElement('div');
         cardImage.id = 'card-image';
         cardImage.classList.add('col-md-3', 'img-fluid');
         cardImage.innerHTML = `<img src="${product.img}" alt="${product.name}">`;
-  
+
         let cardBody = document.createElement('div');
         cardBody.id = 'card-body';
         cardBody.classList.add('col-md-9');
@@ -195,29 +195,29 @@ function filterInstruments(category) {
         cardRow.appendChild(cardImage);
         cardRow.appendChild(cardBody);
         productCard.appendChild(cardRow);
-  
+
         productCard.addEventListener('click', () => {
             cardBody.innerHTML = `<div id="product-details">${product.description}</div>
                             <p id="product-price">€ ${product.price}.00</p>`
         });
         productList.appendChild(productCard);
     });
-  }
-  
-  function renderProductsSelect() {
+}
+
+function renderProductsSelect() {
     let allProducts = INSTRUMENT_PRODUCTS_LIST.concat(AUDIO_PRODUCTS_LIST);
     let selector = document.getElementById('products-selector');
-  
+
     allProducts.forEach(product => {
         let listItem = document.createElement('option')
         listItem.value = product.name;
         listItem.textContent = product.name;
-  
+
         selector.appendChild(listItem)
     });
-  }
-  
-  function validateForm() {
+}
+
+function validateForm() {
     let name = document.getElementById('name').value;
     let phone = document.getElementById('phone').value;
     let email = document.getElementById('email').value;
@@ -225,29 +225,29 @@ function filterInstruments(category) {
     let select = document.getElementById('products-selector').value;
     let error = document.getElementById('error')
     const form = document.getElementById('rental-form');
-  
+
     form.addEventListener('submit', (e) => {
-      let messages = [];
+        let messages = [];
   
-      if (name === "" || name == null) {
-      messages.push("You must input a Name");
-      }
-      if (phone.length < 10) {
-      messages.push("Phone number must be 10 digits or More");
-      }
-      if (email === "") {
-      messages.push("Your Email must be included");
-      }
-      if (date === "") {
+        if (name === "" || name == null) {
+        messages.push("You must input a Name");
+        }
+        if (phone.length < 10) {
+        messages.push("Phone number must be 10 digits or More");
+        }
+        if (email === "") {
+        messages.push("Your Email must be included");
+        }
+        if (date === "") {
         messages.push("You must select at valid date");
-      }
-      if (select === "") {
-      messages.push("You must select at least one item");
-      }
-      
-      if (messages.length > 0) {
-      e.preventDefault();
-      error.innerHTML = messages.join('<br>');
-      }
-      });
-    }
+        }
+        if (select === "") {
+        messages.push("You must select at least one item");
+        }
+
+        if (messages.length > 0) {
+        e.preventDefault();
+        error.innerHTML = messages.join('<br>');
+        }
+    });
+}
